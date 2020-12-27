1c4a26440572ef84642ffb2f214ffae50999984c
Kyler Wong <wsm.kyler@gmail.com>
2020-12-27 17:09:47 +0800

    feat: update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ const Home: React.FC = () => {
     <PageContainer>
       <Head>
         <title>ReadyAnot</title>
-        <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
--- pages/privilege-test.tsx
+++ pages/privilege-test.tsx
@@ -157,7 +157,6 @@ const PrivilegeTest: React.FC = () => {
     <PageContainer>
       <Head>
         <title>Privilege Test</title>
-        <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
