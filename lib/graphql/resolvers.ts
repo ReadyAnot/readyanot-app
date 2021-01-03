d937bd9fc34d264687e8ae558db630749da59fbb
Kyler Wong <wsm.kyler@gmail.com>
2021-01-04 02:25:55 +0800

    feat: add placeholder queries

--- lib/graphql/resolvers.ts
+++ lib/graphql/resolvers.ts
@@ -3,6 +3,11 @@ import { IResolvers } from 'apollo-server-micro'
 const resolvers: IResolvers = {
   Query: {
     pingServer: () => true,
+    getQuizQuestions: async () => [],
+  },
+
+  Mutation: {
+    logLabelData: async () => true,
   },
 }
 
--- lib/graphql/schema.ts
+++ lib/graphql/schema.ts
@@ -3,6 +3,11 @@ import { gql } from 'apollo-server-micro'
 const typeDefs = gql`
   type Query {
     pingServer: Boolean!
+    getQuizQuestions: [String!]!
+  }
+
+  type Mutation {
+    logLabelData(label: String!, data: String!): Boolean!
   }
 `
 
