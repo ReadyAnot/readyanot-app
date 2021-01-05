f4a00e9a24f61d21acea8ac75592ef61e5f998d4
Kyler Wong <wsm.kyler@gmail.com>
2021-01-05 17:38:06 +0800

    fix: resolve execution errors

--- lib/aws/secrets.ts
+++ lib/aws/secrets.ts
@@ -4,14 +4,25 @@ export enum Secret {
   Postgres = 'prod/ReadyAnot/PostgreSQL',
 }
 
+export type PostgresSecretType = {
+  username: string
+  password: string
+  engine: string
+  host: string
+  port: number
+  dbInstanceIdentifier: string
+}
+
 const client = new AWS.SecretsManager({ region: 'ap-southeast-1' })
 
-const getAwsSecret = async (secretName: string) => {
+const getAwsSecret = async (secretName: Secret) => {
   try {
     const result = await client
       .getSecretValue({ SecretId: secretName })
       .promise()
     if (result.SecretString) {
+      const secret = JSON.parse(result.SecretString)
+      return secret
     }
   } catch (err) {
     console.error(`Error fetching AWS secret [${secretName}]: ${err.code}`)
