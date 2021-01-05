b5d67af645e096b5a76296272cd03f41fca11fd4
Kyler Wong <wsm.kyler@gmail.com>
2021-01-05 17:50:42 +0800

    fix: bring back docker build command

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -5,7 +5,7 @@ AWS_ECR_URL="363599773651.dkr.ecr.ap-southeast-1.amazonaws.com"
 
 set -e
 
-# docker build -t $APP_NAME .
+docker build -t $APP_NAME .
 docker tag $APP_NAME $AWS_ECR_URL/$APP_NAME
 docker login -u AWS -p $(aws ecr get-login-password --region ap-southeast-1) $AWS_ECR_URL
 docker push $AWS_ECR_URL/$APP_NAME
