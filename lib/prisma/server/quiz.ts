600551e11b1c6e5872ccd74b9cbb9d8beb22bebc
Kyler Wong <wsm.kyler@gmail.com>
2021-02-19 13:32:37 +0800

    refactor: remove server-side components

--- lib/prisma/server/quiz.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '..'
-
-const getQuizQuestions = async () => {
-  return await prisma.quizQuestion.findMany({
-    orderBy: { id: 'asc' },
-  })
-}
-
-export default getQuizQuestions
