9e0431aaed689e1813f2104ca64656f0058d45da
Wei Bin <auyeongweibin@gmail.com>
2021-07-31 05:13:13 +0800

    Added CORS to candid.sg

--- pages/privilege-test.tsx
+++ pages/privilege-test.tsx
@@ -117,7 +117,6 @@ const PrivilegeTest: NextPage<PrivilegeTestProps> = ({ questions }) => {
       body: JSON.stringify(logObject),
       mode: 'cors',
     })
-    console.log(JSON.stringify(logObject))
     setShowResults(true)
   }
 
